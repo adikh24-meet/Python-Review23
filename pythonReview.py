399963a87f0be2d19a9317ee1e45dfc37f99a4b7
adikh24-meet <139681014+adikh24-meet@users.noreply.github.com>
2023-07-16 18:58:44 +0300

    Create pythonReview.py

---
+++ pythonReview.py
@@ -0,0 +1,20 @@
+def create_youtube_video(title,description):
+	youtube = {"title":title,"discription":discription,"likes":0,"dislikes":0,"comments":{}}
+	return youtube
+
+def like(youtube):
+	if "likes" in youtube:
+		youtube[likes]++
+	return youtube
+
+def like(youtube):
+	if "dislikes" in youtube:
+		youtube[likes]+=1
+	return youtube
+
+def add_comment(youtube,username,comment_text):
+	youtube[comments]={username:comment_text}
+	return youtube
+
+youtubevid = create_youtube_video("ta3arof","hello guys how are you?")
+
