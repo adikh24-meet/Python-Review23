fda3487cb8b012b845ae95eebc2e190c436d1be5
adikh24-meet <139681014+adikh24-meet@users.noreply.github.com>
2023-07-19 11:51:20 +0300

    Update pythonReview.py

--- pythonReview.py
+++ pythonReview.py
@@ -1,13 +1,14 @@
-def create_youtube_video(title,description):
+def create_youtube_video(title,discription):
 	youtube = {"title":title,"discription":discription,"likes":0,"dislikes":0,"comments":{}}
 	return youtube
+
 
 def like(youtube):
 	if "likes" in youtube:
 		youtube[likes]+=1
 	return youtube
 
-def like(youtube):
+def dislike(youtube):
 	if "dislikes" in youtube:
 		youtube[likes]+=1
 	return youtube
@@ -17,4 +18,4 @@ def add_comment(youtube,username,comment_text):
 	return youtube
 
 youtubevid = create_youtube_video("ta3arof","hello guys how are you?")
-
+print (youtubevid)
