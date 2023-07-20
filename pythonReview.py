65b5fa1589553b95e7ad6b26d176406e416ad4f1
adikh24-meet <139681014+adikh24-meet@users.noreply.github.com>
2023-07-20 16:24:50 +0300

    Update pythonReview.py

--- pythonReview.py
+++ pythonReview.py
@@ -1,5 +1,5 @@
-def create_youtube_video(title,discription):
-	youtube = {"title":title,"discription":discription,"likes":0,"dislikes":0,"comments":{}}
+def create_youtube_video(title,discription,hashtag):
+	youtube = {"title":title,"discription":discription,"likes":0,"dislikes":0,"comments":{},"hashtag":hashtag}
 	return youtube
 
 
@@ -17,11 +17,25 @@ def add_comment(youtube,username,comment_text):
 	youtube["comments"]={username:comment_text}
 	return youtube
 
-youtubevid = create_youtube_video("ta3arof","hello guys how are you?")
+def similarity_to_video(youtube1,youtube2):
+	count=0
+	list1 = youtube1["hashtag"]
+	list2 = youtube2["hashtag"]
+	for i in range(5):
+		for hash in list1:
+			if hash == list2[i]:
+				count+=1
+	print(count,"/ 5")
+
+youtubevid1 = create_youtube_video("ta3arof","hello guys how are you?",["fun","cute","funny","cat","animal"])
+youtubevid2 = create_youtube_video("ta3arof","hello guys how are you?",["fun","cute","funny","dog","animal"])
+
 
 for i in range(495):
-	like(youtubevid)
+	like(youtubevid1)
 
-add_comment(youtubevid,"adi","you nawartoo paris")
+add_comment(youtubevid1,"adi","you nawartoo paris")
 
-print(youtubevid)
+print(youtubevid1)
+
+similarity_to_video(youtubevid1,youtubevid2)
